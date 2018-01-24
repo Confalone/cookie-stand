849d17762c1b870562d11d75e2ed633ebc81c041
Tyler <Tconfalone@gmail.com>
2018-01-24 11:15:42 -0800

    push3

--- sales.js
+++ sales.js
@@ -3,9 +3,42 @@
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var store = {
-    location = ,
-    minCust = ,
-    maxCust = ,
-    avgCook = ,
-
+    location = '1st and Pike',
+    minCust = 23,
+    maxCust = 65,
+    avgCook = 6.3,
+    function getRandomInt(minCust, maxCust) {
+        min = Math.ceil()
+    }
 }
+
+var store = {
+    location = 'SeaTac Airport',
+    minCust = 3,
+    maxCust = 24,
+    avgCook = 1.2,
+}
+
+var store = {
+    location = 'Seatle Center',
+    minCust = 11,
+    maxCust = 38,
+    avgCook = 3.7,
+}
+
+var store = {
+    location = 'Capitol Hill',
+    minCust = 20,
+    maxCust = 38,
+    avgCook = 2.3,
+}
+
+var store = {
+    location = 'Alki',
+    minCust = 2,
+    maxCust = 16,
+    avgCook = 4.6,
+}
+//function getRandomIntInclusive
+
+
